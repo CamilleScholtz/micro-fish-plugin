00e96c880404af26d99457433e492153c14e0d4b
Camille Scholtz <onodera@openmailbox.org>
2016-10-06 19:52:03 +0200

    initial commit

---
+++ fish.lua
@@ -0,0 +1,24 @@
+VERSION = "0.1"
+
+if GetOption("fishfmt") == nil then
+    AddOption("fishfmt", false)
+end
+
+MakeCommand("fishfmt", "fish.fishfmt", 0)
+
+function onSave(view)
+    if CurView().Buf:FileType() == "fish" then
+        if GetOption("fishfmt") then
+            fishfmt()
+        end
+    end
+end
+
+function fishfmt()
+    CurView():Save(false)
+    local handle = io.popen("fish_indent -w " .. CurView().Buf.Path)
+    local result = handle:read("*a")
+    handle:close()
+    
+    CurView():ReOpen()
+end
